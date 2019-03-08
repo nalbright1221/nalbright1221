69c81faf7b366630faf446bbebfa782d54fb8134
Nicole <nalbrigh@kent.edu>
2019-03-07 22:26:21 -0500

    io push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,166 +1,108 @@
-//array with objects containing questions/answers/correct answer 
-var questions = [{
-        question: 'What is the capital of Italy?',
-        answers: ['Milan', 'Turin', 'Rome', 'Naples'],
-        correctAnswer: 'Rome',
-        image:  ''
-    },
+//create an array of strings 
+var topics = ['angry', ' bored', ' disappointed', ' drunk', ' embarassed', ' excited', ' frustrated', ' happy', ' hungry', ' inpsired', ' lonely', ' love', ' nervous', ' relaxed', ' sad', ' sassy', ' scared', ' shocked', ' sick', ' tired'];
 
-    {
-        question: 'Who painted The Last Supper?',
-        answers: ['Michaelangelo', 'Caravagio', 'Giotto', 'Leonardo da Vinci'],
-        correctAnswer: 'Leonardo da Vinci',
-        image: ''
-    },
+//function that creates buttons
+function buttons() {
+    $("#buttons").empty();
 
-    {
-        question: 'What was the name of the town destroyed by mount Vesuvius in 79AD?',
-        answers: ['Pompei', 'Sorrento', 'Paestum', 'Agropoli'],
-        correctAnswer: 'Pompei',
-        image: '',
-    },
-    {
-        question: 'What is the name of the church in the Vatican?',
-        answers: ['St Pauls', 'St Marks', 'St Peters', 'St Johns'],
-        correctAnswer: 'St Peters',
-        image: '',
-    },
-    {
-        question: 'Who painted the ceiling of the Sistine Chapel?',
-        answers: ['Titian', 'Michaelangelo', 'Donatello', 'Raphael'],
-        correctAnswer: 'Michaelangelo',
-        image: '',
-    },
-    {
-        question: 'Which roman emperor was assassinated on the Ides of March?',
-        answers: ['Julius', 'Augustus', 'Nero', 'Octavius'],
-        correctAnswer: 'Julius',
-        image: '/images/ceasar.jpg'
+    for (var i = 0; i < topics.length; i++) {
+        var newButtons = $("<button>");
+        newButtons.text(topics[i]);
+        newButtons.addClass("clickMeForGif")
+        $("#buttons").append(newButtons);
     }
-];
-
-//global variable for
-//wins/losses/unchosen answer
-//timer 
-//current question 
-var wins = 0;
-var losses = 0;
-var notChosen = 0;
-var number = 31;
-var timeSpacer = 6;
-var intervalId;
-var currentQuestion = 0;
-
-//waits until the document is ready to load
-//hides the timer until start button is clicked
-//hides the questions until start button is clicked 
-$(document).ready(function () {
-    $("#start").show();
-    $("timer").hide();
-    $("#question-answers").hide();
-})
-
-//end game function will show player how many answers they got right or wrong 
-var endGame = function () {
-    $("#question-answers").hide();
-    $("timer").hide();
-    $("#wins").text("Answered Correctly: " + wins);
-    $("#losses").text("Answered Incorrectly: " + losses);
-    $("#not-answered").text("Answered Not Chosen: " + notChosen);
 }
 
-//user clicks start button 
-$("#start").on("click", function () {
-    //hides start button once its been clicked 
-    $('#start').attr("hidden", true);
-    //timer and questions and answers shown
-    $("timer").show();
-    $("#question-answers").show();
-    run();
-    //calls function to show first question 
-    setQuestion(currentQuestion);
+//call buttons function to generate buttons listed in array 
+buttons();
+
+//this click event runs when user hits sumbit or enter after typing in new button value 
+$("#userSubmit").on("click", function (event) {
+    //prevents page from reloading everytime sumbit button is  clicked 
+    event.preventDefault();
+    //variable that takes the value of new button user created 
+    var newEmotion = $("#newEmotionButton").val().trim();
+    //pushes new emotion button to our button topics array
+    topics.push(newEmotion);
+    //clear input 
+    $("#newEmotionButton").val('');
+    //call buttons function
+    buttons();
 });
 
-//timer function - runs timer from 31 seconds down 
-function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-}
+//function 
 
-//  The decrement function.
-function decrement() {
 
-    //  Decrease seconds by 1 second 
-    number--;
+//on click event
+//allows user to click botton and gernerate gifs 
+$(document).on("click", ".clickMeForGif", function () {
+    var a = $(this).text();
 
-    //  Show the number in the #seconds-remaining in html.
-    $("#seconds-remaining").html("Seconds Remaining: " + number);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + a + "&api_key=5OMJTXCR27pKXP4E9xeUel3ubAzaXQu8&limit=10";
 
-    if (number === 0) {
+    //calls out to get information 
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
 
-        //if timer gets to 0, the next question will load 
-        currentQuestion++;
-        setQuestion(currentQuestion);
+        //retrieves information 
+        .then(function (response) {
 
-        number = 31;
+            var results = response.data;
+            console.log(response);
 
-        //increments not answered score by 1 
-        notChosen++;
-        // $("#result").text("Mi dispiace... You didnt choose an answer. The correct answer was " + questions[currentQuestion].correctAnswer);
+            //loops through results taken from  giphy API
+            for (var i = 0; i < results.length; i++) {
+                console.log(results[i]);
+
+                //variable creating new div 
+                var gifDiv = $("<div>");
+
+                //variable that gets the rating of gif 
+                var rating = results[i].rating;
+
+                //creates a paragraph and puts text into tag 
+                var p = $("<p>").text("Rating: " + rating);
+
+                //varriable that  creates image tag 
+                var personImage = $("<img>");
+
+
+                personImage.attr("data-still", results[i].images.original_still.url);
+                personImage.attr("data-animate", results[i].images.original.url);
+                personImage.attr("data-state", "still");
+                personImage.attr("src", results[i].images.original_still.url);
+
+                //adds class to gif image 
+                personImage.addClass("pausePlay");
+
+                //appends paragraph tag(rating) to div 
+                gifDiv.append(p);
+
+                //appends image tag to  div 
+                gifDiv.append(personImage);
+
+                //puts givDif into gifSection in HTML
+                $("#gifSection").append(gifDiv);
+            }
+        });
+});
+
+//click event that allows user to click to animate or still the gif 
+$(document).on("click", ".pausePlay", function () {
+
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    console.log(state);
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
     }
-}
-
-
-
-
-function stop() {
-    //  Clears our intervalId
-    //  We just pass the name of the interval
-    //  to the clearInterval function.
-    clearInterval(intervalId);
-}
-
-//puts questions and answers into html placement 
-var setQuestion = function (index) {
-    $('#question').text(questions[index].question);
-    $('#answer1').text(questions[index].answers[0]);
-    $('#answer2').text(questions[index].answers[1]);
-    $('#answer3').text(questions[index].answers[2]);
-    $('#answer4').text(questions[index].answers[3]);
-
-    //resets timer back to 31 
-    number = 31;
-}
-
-
-//on  click function that  logs  whatever answer has been chosen 
-$(".answer").on("click", function () {
-
-    //this gets the text from the button clicked 
-    $(this).text()
-
-    //compares and increments answers correctly answered 
-    if ($(this).text() === questions[currentQuestion].correctAnswer) {
-        wins++;
-        // $("#result").text("Bellissimo! You answered Correctly!");
-        // $("#image").text('<img src=' + questions[currentQuestion].image + '/>');
-    }
-
-    //this works if you select the correct answer 
-    
-    //compares and increments answers not correctly answered  
-    else {
-        losses++;        
-        // $("#result").text("Mi dispiace...  The correct answer was " + questions[currentQuestion].correctAnswer);
-    }
-    
-    if (currentQuestion === 5) {
-        $("#seconds-remaining").hide();
-
-        endGame();
-    }
-    //moves on to the nest question 
-    // setTimeout(2000);
-    currentQuestion++;
-    setQuestion(currentQuestion).delay( 800 );
 });
