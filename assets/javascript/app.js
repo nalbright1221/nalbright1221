a18472614f289bc51cb1d44f7251f37908a1c46c
Nicole <nalbrigh@kent.edu>
2019-03-14 23:30:55 -0400

    final push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,108 +1,118 @@
-//create an array of strings 
-var topics = ['angry', ' bored', ' disappointed', ' drunk', ' embarassed', ' excited', ' frustrated', ' happy', ' hungry', ' inpsired', ' lonely', ' love', ' nervous', ' relaxed', ' sad', ' sassy', ' scared', ' shocked', ' sick', ' tired'];
+//counter
+var count = 0;
 
-//function that creates buttons
-function buttons() {
-    $("#buttons").empty();
+// information needed to connect to firebase database 
+var config = {
+    apiKey: "AIzaSyD9UxykL2Aix6XolHwDuswjUQWoDGyXj4Y",
+    authDomain: "train-schedule-wk7-11d97.firebaseapp.com",
+    databaseURL: "https://train-schedule-wk7-11d97.firebaseio.com",
+    projectId: "train-schedule-wk7-11d97",
+    storageBucket: "train-schedule-wk7-11d97.appspot.com",
+    messagingSenderId: "16070555178"
+  };
+  firebase.initializeApp(config);
 
-    for (var i = 0; i < topics.length; i++) {
-        var newButtons = $("<button>");
-        newButtons.text(topics[i]);
-        newButtons.addClass("clickMeForGif")
-        $("#buttons").append(newButtons);
-    }
-}
+var database = firebase.database();
 
-//call buttons function to generate buttons listed in array 
-buttons();
 
-//this click event runs when user hits sumbit or enter after typing in new button value 
-$("#userSubmit").on("click", function (event) {
-    //prevents page from reloading everytime sumbit button is  clicked 
+//form on click function
+//preventDefault prevent page from reloading 
+$("#add-employee-btn").on("click", function () {
     event.preventDefault();
-    //variable that takes the value of new button user created 
-    var newEmotion = $("#newEmotionButton").val().trim();
-    //pushes new emotion button to our button topics array
-    topics.push(newEmotion);
-    //clear input 
-    $("#newEmotionButton").val('');
-    //call buttons function
-    buttons();
+
+//takes values from input boxes and trims them 
+    name = $("#employee-name-input").val().trim();
+    destination = $("#role-input").val().trim();
+    arrivalTime = $("#start-input").val().trim();
+    frequency = $("#rate-input").val().trim();
+
+    //set count to 0
+    count = 0;
+
+    console.log(name);
+    console.log(destination);
+    console.log(arrivalTime);
+    console.log(frequency);
+
+    //pushes information in firebase with correct variables 
+    database.ref().push({
+        name: name,
+        destination: destination,
+       arrivalTime: arrivalTime,
+       frequency: frequency,
+       
+    });
+
+    //alerts the user they successfully added a train schedule 
+    alert("New Train is on the way!");
+    
+//empties  the inut box value 
+    $("#employee-name-input").val("");
+    $("#role-input").val("");
+    $("#start-input").val("");
+    $("#rate-input").val("");
 });
 
-//function 
+
+//
+database.ref().on("child_added", function (document){
+   //sets counter equal to 1 
+    count += 1;
+
+    console.log(document.key);
+    console.log(document.val());
+
+    var name = document.val().name;
+    var destination = document.val().destination;
+    var arrivalTime = document.val().arrivalTime;
+    var frequency = document.val().frequency;
+    var trainArrivalMinutes;
+    var trainArrivalTime;
+    var timeArray = arrivalTime.split(":");
+    var now = moment();
+
+    console.log(arrivalTime);
+
+    // var time = moment(arrivalTime, "HH:MM").subtract(1, "years");
+    var time = moment().hours(timeArray[0]).minutes(timeArray[1]);
 
 
-//on click event
-//allows user to click botton and gernerate gifs 
-$(document).on("click", ".clickMeForGif", function () {
-    var a = $(this).text();
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + a + "&api_key=5OMJTXCR27pKXP4E9xeUel3ubAzaXQu8&limit=10";
+    //your problems is right below 
+    var difference =  now.diff(time, "minutes");
+    var total = difference % frequency;
+    trainArrivalMinutes =  frequency - total;
 
-    //calls out to get information 
-    $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-
-        //retrieves information 
-        .then(function (response) {
-
-            var results = response.data;
-            console.log(response);
-
-            //loops through results taken from  giphy API
-            for (var i = 0; i < results.length; i++) {
-                console.log(results[i]);
-
-                //variable creating new div 
-                var gifDiv = $("<div>");
-
-                //variable that gets the rating of gif 
-                var rating = results[i].rating;
-
-                //creates a paragraph and puts text into tag 
-                var p = $("<p>").text("Rating: " + rating);
-
-                //varriable that  creates image tag 
-                var personImage = $("<img>");
+    console.log(difference);
+    console.log(total);
+     console.log(time);
+     
+     
 
 
-                personImage.attr("data-still", results[i].images.original_still.url);
-                personImage.attr("data-animate", results[i].images.original.url);
-                personImage.attr("data-state", "still");
-                personImage.attr("src", results[i].images.original_still.url);
+var nextTrain = moment().add(trainArrivalMinutes, "minutes");
+trainArrivalTime = moment(nextTrain).format("HH:MM");
 
-                //adds class to gif image 
-                personImage.addClass("pausePlay");
+var a = "<a href=# onclick=deleteDocument('" + document.key + "');>Delete</a>";
 
-                //appends paragraph tag(rating) to div 
-                gifDiv.append(p);
+//appends new train information into table 
+$("#employee-table").append(
+    $("<tr>").append(
+        $("<td>").text(name),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(nextTrain.format("hh:mm")),
+        $("<td>").text(trainArrivalMinutes),
+        $("<td>").html(a)
+    ));
 
-                //appends image tag to  div 
-                gifDiv.append(personImage);
+console.log("Record:" + count);
 
-                //puts givDif into gifSection in HTML
-                $("#gifSection").append(gifDiv);
-            }
-        });
 });
 
-//click event that allows user to click to animate or still the gif 
-$(document).on("click", ".pausePlay", function () {
-
-    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-    var state = $(this).attr("data-state");
-    console.log(state);
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
-    if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-    } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
-});
+//allows the user to delete train schedule information from both viewer and database 
+function deleteDocument(documentId) {
+    database.ref().child(documentId).set(null);
+    alert("Train successfully deleted!");
+    location.reload();
+}
